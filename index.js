e386f434d045899d936d99c7fad9518a4775f460
thisispiggy <thisispiggy@gmail.com>
2020-08-30 09:56:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const os = require('os')
+const os = require("os");
 const path = require("path");
 const fs = require("fs");
 const commander = require("commander");
@@ -27,15 +27,18 @@ commander
   .parse(process.argv);
 
 // sets md and image files
-console.log(rootDir, commander.path, attachmentFolder, ankiProfile)
 
 let files = glob.sync(path.join(commander.path, "*.md"));
 let imgs = glob.sync(path.join(attachmentFolder, "*.{png,jpg,jpeg,gif}"));
 let imageURL = /(?<=!\[.+\]\()(?!http)(.+)(?=.)/g;
-if (os.platform() == 'win') {
-  const ankiPath = path.join(process.env.APPDATA, ankiProfile);
+if (os.platform() === "win32") {
+  var ankiPath = path.join(process.env.APPDATA, ankiProfile);
 } else {
-  const ankiPath = path.join(process.env.HOME, '/Library/Application Support/', ankiProfile);
+  var ankiPath = path.join(
+    process.env.HOME,
+    "/Library/Application Support/",
+    ankiProfile
+  );
 }
 
 // defines output html file
